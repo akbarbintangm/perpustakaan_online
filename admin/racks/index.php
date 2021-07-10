eab5994347435fcbc5873488ed548f53691cecd4
akbarbintangm <akbarbintangmahendra@gmail.com>
2021-07-10 21:55:05 +0700

    update

--- admin/racks/index.php
+++ admin/racks/index.php
@@ -1,6 +1,6 @@
 <?php
-    require_once "../configurations/index.php";
-    require_once "./templates/header.php";
+    require_once "../../configurations/index.php";
+    require_once "../templates/header.php";
 ?>
 
 <body class="animate__animated bg-light text-dark" id="page-top">
@@ -8,7 +8,7 @@
     <!-- Page Wrapper -->
     <div id="wrapper">
 
-        <?php require_once "./templates/sidebar.php"; ?>
+        <?php require_once "../templates/sidebar.php"; ?>
         
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
@@ -16,21 +16,22 @@
             <!-- Main Content -->
             <div id="content">
 
-                <?php require_once "./templates/navbar.php"; ?>
+                <?php require_once "../templates/navbar.php"; ?>
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Daftar Rak</h1>
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm disabled"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
 
                     <?php
-                        require_once "./templates/display_dashboard.php";
+                        require_once "../templates/display_dashboard.php";
                         //require_once "./templates/area_charts.php";
+                        require_once "../templates/racks_table.php";
                     ?>
                     
                 </div>
@@ -40,7 +41,7 @@
             <!-- End of Main Content -->
 
             <?php
-                require_once "./templates/footer_copyrighted.php";
+                require_once "../templates/footer_copyrighted.php";
             ?>
 
         </div>
@@ -55,10 +56,10 @@
     </a>
 
     <?php
-        require_once "./templates/modal_logout.php";
+        require_once "../templates/modal_logout.php";
     ?>
 </body>
 
 <?php
-    require_once "./templates/footer.php";
+    require_once "../templates/footer.php";
 ?>
--- admin/templates/sidebar.php
+++ admin/templates/sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= BASEURL ?>/admin/">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-book"></i>
         </div>
@@ -14,7 +14,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="<?= BASEURL ?>/admin/">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
@@ -24,55 +24,28 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Buku
+        Divisi Buku
     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
+    <!-- Nav Item - Books -->
+    <li class="nav-item active">
+        <a class="nav-link" href="<?= BASEURL ?>/admin/books/">
             <i class="fas fa-fw fa-book"></i>
-            <span>Daftar Buku</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
-            </div>
-        </div>
+            <span>Books</span></a>
     </li>
     
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree"
-            aria-expanded="true" aria-controls="collapseThree">
+    <!-- Nav Item - Racks -->
+    <li class="nav-item active">
+        <a class="nav-link" href="<?= BASEURL ?>/admin/racks/">
             <i class="fas fa-fw fa-box"></i>
-            <span>Daftar Rak</span>
-        </a>
-        <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
-            </div>
-        </div>
+            <span>Racks</span></a>
     </li>
     
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour"
-            aria-expanded="true" aria-controls="collapseFour">
+    <!-- Nav Item - Category -->
+    <li class="nav-item active">
+        <a class="nav-link" href="<?= BASEURL ?>/admin/category/">
             <i class="fas fa-fw fa-archive"></i>
-            <span>Daftar Kategori</span>
-        </a>
-        <div id="collapseFour" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
-            </div>
-        </div>
+            <span>Category</span></a>
     </li>
     
     <!-- Divider -->
@@ -80,26 +53,14 @@
     
     <!-- Heading -->
     <div class="sidebar-heading">
-        User
+        Divisi User
     </div>
     
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-            aria-expanded="true" aria-controls="collapseUtilities">
+    <!-- Nav Item - Users -->
+    <li class="nav-item active">
+        <a class="nav-link" href="<?= BASEURL ?>/admin/users/">
             <i class="fas fa-fw fa-users"></i>
-            <span>Daftar User</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-            data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
-            </div>
-        </div>
+            <span>Users</span></a>
     </li>
     
     <!-- Divider -->
